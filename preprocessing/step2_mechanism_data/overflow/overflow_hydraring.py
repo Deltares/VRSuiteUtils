74243d2623414e00aff9a6b86815b824860448a2
WJKlerk <61802340+WJKlerk@users.noreply.github.com>
2023-04-18 13:24:06 +0200

    feature: Modified class names. Add configpath as variable for inifile. Added consistent naming test data and results

--- preprocessing/step2_mechanism_data/overflow/overflow_hydraring.py
+++ preprocessing/step2_mechanism_data/overflow/overflow_hydraring.py
@@ -12,19 +12,21 @@ from pathlib import Path
 import sqlite3
 import subprocess
 
-class HydraRingComputationInput:
+class HydraRingComputation:
     def __init__(self,HRING_path = r"C:\Program Files (x86)\BOI\Riskeer 21.1.1.2\Application\Standalone\Deltares\HydraRing-20.1.3.10236\MechanismComputation.exe"):
         #default HRING_path is to latest Riskeer version.
         self.HydraRingPath =  Path(HRING_path)
+
+
 
     def run_hydraring(self,
             inifile: Path,
     ):
         #TODO aanpassen
-        subprocess.run([self.HydraRingPath, str(inifile)], cwd=str(inifile.parent))
+        subprocess.run([str(self.HydraRingPath), str(inifile)], cwd=str(inifile.parent))
 
 
-class OverflowComputationInput(HydraRingComputationInput):
+class OverflowComputationInput(HydraRingComputation):
     def get_critical_discharge(self,discharge_path):
         critical_discharges = pd.read_csv(discharge_path,index_col=0)
         try:
@@ -166,12 +168,16 @@ class OverflowComputationInput(HydraRingComputationInput):
             else:
                 sys.stdout.write(line)
         pass
-    def make_ini_file(self,path,reference_file,db_path):
+    def make_ini_file(self,path,reference_file,db_path,config_db_path):
+        if config_db_path.name != 'config.sqlite':
+            config_db_path = config_db_path.joinpath('config.sqlite')
         new_ini = path.joinpath(self.name + '.ini')
         shutil.copy(reference_file,new_ini)
 
         for j, line in enumerate(fileinput.input(new_ini, inplace=1)):
             sys.stdout.write(line.replace('DIJKPAAL', self.name))
         for j, line in enumerate(fileinput.input(new_ini, inplace=1)):
-            sys.stdout.write(line.replace('DATABASEPATH', str(db_path)))
+            sys.stdout.write(line.replace('DATABASEPATH', str(Path(os.getcwd()).joinpath(db_path))))
+        for j, line in enumerate(fileinput.input(new_ini, inplace=1)):
+            sys.stdout.write(line.replace('CONFIGDBPATH', str(config_db_path)))
         self.ini_path = new_ini
--- tests/test_step_2_generate_mechanism_input.py
+++ tests/test_step_2_generate_mechanism_input.py
@@ -5,15 +5,16 @@ from preprocessing.step1_generate_shapefile.traject_shape import TrajectShape
 from pathlib import Path
 import geopandas as gpd
 import pandas as pd
+import os
 from geopandas.testing import assert_geodataframe_equal,assert_geoseries_equal
 from preprocessing.step2_mechanism_data.overflow.overflow_input import OverflowInput
-from preprocessing.step2_mechanism_data.overflow.overflow_hydraring import OverflowComputationInput
+from preprocessing.step2_mechanism_data.overflow.overflow_hydraring import OverflowComputationInput, HydraRingComputation
 @pytest.mark.parametrize("hring_input,gekb_shape,traject_shape,db_location",[(Path('test_data').joinpath('38-1','input','HRING_data.csv'),
                                                                   Path('test_data').joinpath('38-1','input','gekb_shape.shp'),
                                                                   Path('test_data').joinpath('38-1','reference_shape.shp'),
                                                                   Path('test_data').joinpath('38-1','input','db','2023','WBI2017_Bovenrijn_38-1_v04.sqlite'))])
 def test_select_HRING_locs(hring_input,traject_shape,db_location,gekb_shape):
-    #HRING_Input input should have: M-value of location, and what is in HR-data.
+    #HRING_input input should have: M-value of location, and what is in HR-data.
     #read GEKB_input:
     hring_df = pd.read_csv(hring_input,index_col=0)
     gekb_shape = gpd.read_file(gekb_shape).sort_values('M_VAN').drop('OBJECTID', axis=1).reset_index(drop=True).reset_index().rename(columns={'index': 'OBJECTID'})
@@ -32,15 +33,17 @@ def test_select_HRING_locs(hring_input,traject_shape,db_location,gekb_shape):
     overflow_input.verify_and_filter_columns()
     pd.testing.assert_frame_equal(overflow_input.hring_data,pd.read_csv(Path(traject_shape.parent).joinpath('HRING_data_reference.csv'),index_col=0))
 
-@pytest.mark.parametrize("HRING_data,HRING_reference_files, prfl_path, db_path, results_dir, ilocs",[(Path('test_data').joinpath('38-1','HRING_data_reference.csv'),
-                                                                               Path('test_data').joinpath('38-1','HRING_reference_files'),
+@pytest.mark.parametrize("HRING_data,HRING_input_reference, prfl_path, db_path, results_dir, ilocs",[(Path('test_data').joinpath('38-1','HRING_data_reference.csv'),
+                                                                               Path('test_data').joinpath('38-1','HRING_input_reference'),
                                                                                Path('test_data').joinpath('38-1','input','prfl'),
                                                                                Path('test_data').joinpath('38-1','input','db'),
-                                                                               Path('test_results').joinpath('38-1','HRING_Input'),
+                                                                               Path('test_results').joinpath('38-1','HRING_input'),
                                                                                [0, 20, 40])])
-def test_make_HRING_overflow_input(HRING_data,HRING_reference_files,prfl_path, results_dir, db_path,ilocs):
+def test_make_HRING_overflow_input(HRING_data,HRING_input_reference,prfl_path, results_dir, db_path,ilocs,
+                                   config_db_path = Path(r'c:\Program Files (x86)\BOI\Riskeer 21.1.1.2\Application\Standalone\Deltares\HydraRing-20.1.3.10236\config.sqlite')):
     #based on HRING_data_reference:
     #generate input SQL and ini files for Hydra-Ring
+
     database_paths = list(db_path.glob("*\\"))
 
     #clear the output dir
@@ -59,7 +62,7 @@ def test_make_HRING_overflow_input(HRING_data,HRING_reference_files,prfl_path, r
             loc_output_dir = results_dir.joinpath(database_path.stem,location.dijkvak)
             loc_output_dir.mkdir(parents=True,exist_ok=False)
 
-            loc_output_reference_dir = HRING_reference_files.joinpath(database_path.stem,location.dijkvak)
+            loc_output_reference_dir = HRING_input_reference.joinpath(database_path.stem,location.dijkvak)
             computation = OverflowComputationInput()
             #data from input sheet:
             computation.fill_data(location)
@@ -74,16 +77,33 @@ def test_make_HRING_overflow_input(HRING_data,HRING_reference_files,prfl_path, r
             if not filecmp.cmp(loc_output_dir.joinpath(computation.name + '.sql'),loc_output_reference_dir.joinpath(computation.name + '.sql')):
                 comparison_errors_sql.append('SQL-file for {} not identical'.format(computation.name))
 
-            computation.make_ini_file(loc_output_dir,Path('test_data').joinpath('general','ini_reference_overflow.ini'),database_path)
+            computation.make_ini_file(loc_output_dir,Path('test_data').joinpath('general','ini_reference_overflow.ini'),database_path,config_db_path)
             if not filecmp.cmp(loc_output_dir.joinpath(computation.name + '.ini'),loc_output_reference_dir.joinpath(computation.name + '.ini')):
                 comparison_errors_ini.append('ini-file for {} and database {} not identical'.format(computation.name, database_path.stem))
 
     assert not comparison_errors_ini + comparison_errors_sql, f"Errors: {comparison_errors_ini + comparison_errors_sql}"
 
-# @pytest.mark.parametrize("source_path, results_path, loc_names",[()])
-# def test_run_HydraRing():
-#     pass
-#     #test to verify correct run of Hydra-Ring. Note that it requires a specific version of Riskeer that should be installed locally.
+@pytest.mark.parametrize("ini_file",[(Path('test_data').joinpath('38-1', 'HRING_input_reference','2023','RW000','RW000.ini'))])
+def test_run_HydraRing(ini_file):
+    # HydraRingComputation().run_hydraring(Path(os.getcwd()).joinpath(ini_file))
+
+    work_dir = ini_file.parent
+    # move designTable.txt from work_dir to test_results\38-1\HRING_input\2023\RW000\
+    shutil.move(work_dir.joinpath('designTable.txt'),
+                Path(os.getcwd()).joinpath('test_results',work_dir.parts[1],'HRING_computations',*work_dir.parts[3:],'designTable.txt'))
+
+    #remove the other files for which the name is not the same as the ini_file
+    for file in work_dir.glob('*'):
+        if file.stem != ini_file.stem:
+            file.unlink()
+
+    #compare the designTable.txt with the reference file
+    assert filecmp.cmp(Path(os.getcwd()).joinpath('test_results',work_dir.parts[1],'HRING_computations',*work_dir.parts[3:],'designTable.txt'),
+Path(os.getcwd()).joinpath('test_data',work_dir.parts[1],'HRING_computations_reference',*work_dir.parts[3:],'designTable.txt'))
+
+
+
+    #test to verify correct run of Hydra-Ring. Note that it requires a specific version of Riskeer that should be installed locally.
 #     # TODO: see if we need to add it to the repos.
 
 def test_add_piping_tool_input():
