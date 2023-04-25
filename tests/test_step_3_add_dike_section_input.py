fd117cb12b5665b1d87a8069ce131c8f280c0ad3
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-04-25 21:02:41 +0200

    test: Removed buggy test fixture

--- tests/test_step_3_add_dike_section_input.py
+++ tests/test_step_3_add_dike_section_input.py
@@ -3,8 +3,8 @@ from preprocessing.step1_generate_shapefile.traject_shape import TrajectShape
 from pathlib import Path
 import geopandas as gpd
 from geopandas.testing import assert_geodataframe_equal,assert_geoseries_equal
-@pytest.mark.parametrize("traject,traject_shape",[('38-1',Path('test_data').joinpath('38-1','reference.shp'))])
 
+# @pytest.mark.parametrize("traject,traject_shape",[('38-1',Path('test_data').joinpath('38-1','reference.shp'))])
 def test_derive_toe_from_AHN():
     pass
 
