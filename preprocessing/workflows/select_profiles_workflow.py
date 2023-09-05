eab8fe1fa7761f25644f1bcecc0e1c2d9e7172d0
WJKlerk <wouterjan.klerk@deltares.nl>
2023-09-05 13:36:45 +0200

    chore: also write empty lines when selecting profiles

--- preprocessing/workflows/select_profiles_workflow.py
+++ preprocessing/workflows/select_profiles_workflow.py
@@ -11,13 +11,14 @@ def main_profiel_selectie(
         karakteristieke_profielen: Path,
         profiel_info_csv: Path,
         uitvoer_map: Path,
-        invoerbestand: Path,
-        selectiemethode: str):
+        invoerbestand: Path = False,
+        selectiemethode: str = 'minimum'):
     #make sure uitvoer_map does not exist: delete and recreate
     if uitvoer_map.exists():
         warnings.warn("uitvoer_map already exists. Deleting and recreating")
         #remove with shutil.rmtree
         shutil.rmtree(uitvoer_map)
+
     use_file = False
     uitvoer_map.mkdir()
     #load vakindeling_geojson
@@ -68,6 +69,7 @@ def main_profiel_selectie(
             #plot aggregated profile, and AHN data
             plot_profile(characteristic_profile, vak.vaknaam, available_profiles.csv_filename, ahn_profielen, uitvoer_map)
         else:
+            characteristic_profiles_df.loc[:,vak.vaknaam] = None
             plot_profile(None, vak.vaknaam, available_profiles.csv_filename, ahn_profielen, uitvoer_map)
             warnings.warn(f'No profile found for vak {vak.vaknaam}')
     characteristic_profiles_df.transpose().to_csv(uitvoer_map.joinpath('selected_profiles.csv'), index=True)
@@ -345,17 +347,17 @@ def select_profile(available_profiles, karakteristieke_profielen, section, selec
     return characteristic_profile
 
 if __name__ == '__main__':
-    vakindeling_geojson = Path(r"c:\Repositories\VRSuite\Preprocessing\VrToolPreprocess\tests\test_data\38-1\reference_results\reference_shapes\reference_shape_full.geojson")
-    ahn_profielen = Path(r"c:\Users\klerk_wj\OneDrive - Stichting Deltares\00_Projecten\11_VR_HWBP\test_profielen\test20230814_6\AHN_profiles")
-    karakteristieke_profielen = Path(r"c:\Users\klerk_wj\OneDrive - Stichting Deltares\00_Projecten\11_VR_HWBP\test_profielen\test20230814_6\characteristic_profiles")
-    profiel_info_csv = Path(r"c:\Users\klerk_wj\OneDrive - Stichting Deltares\00_Projecten\11_VR_HWBP\test_profielen\test20230814_6\traject_profiles.csv")
-    uitvoer_map = Path(r"c:\Users\klerk_wj\OneDrive - Stichting Deltares\00_Projecten\11_VR_HWBP\test_profielen\test20230814_6\selectie_profielen2")
-    invoerbestand = Path(r"c:\Users\klerk_wj\OneDrive - Stichting Deltares\00_Projecten\11_VR_HWBP\test_profielen\test20230814_6\invoerbestand.csv")
+    vakindeling_geojson =       Path(r"c:\Veiligheidsrendement\WDOD\teenlijnprofielen\vakindeling_optie1\Vakindeling_53-1.geojson")
+    ahn_profielen =             Path(r"c:\Veiligheidsrendement\WDOD\teenlijnprofielen\profielen\AHN_profiles")
+    karakteristieke_profielen = Path(r"c:\Veiligheidsrendement\WDOD\teenlijnprofielen\profielen\characteristic_profiles")
+    profiel_info_csv =          Path(r"c:\Veiligheidsrendement\WDOD\teenlijnprofielen\profielen\traject_profiles.csv")
+    uitvoer_map =               Path(r"c:\Veiligheidsrendement\WDOD\teenlijnprofielen\profielen\selectie_profielen_1")
+    # invoerbestand =             Path(r"c:\Users\klerk_wj\OneDrive - Stichting Deltares\00_Projecten\11_VR_HWBP\test_profielen\test20230814_6\invoerbestand.csv")
     main_profiel_selectie(
             vakindeling_geojson,
             ahn_profielen,
             karakteristieke_profielen,
             profiel_info_csv,
             uitvoer_map,
-            invoerbestand,
+            False,
             "minimum",)
