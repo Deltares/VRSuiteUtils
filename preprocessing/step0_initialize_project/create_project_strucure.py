eaeece43bfe04b54ec1f7b9e189c40e16dec5400
srikkert <stephan.rikkert@deltares.nl>
2024-04-22 12:59:41 +0200

    Update create_project_strucure.py

--- preprocessing/step0_initialize_project/create_project_strucure.py
+++ preprocessing/step0_initialize_project/create_project_strucure.py
@@ -92,24 +92,24 @@ def create_project_structure(project_folder, traject_id):
         f.write(f"intermediate_results_dir = {intermediate_results_dir} # Intermediate results directory\n")
         f.write(f"vrtool_database_dir = {vrtool_database_dir} # VRTool database directory\n")
 
-        f.write(f"hr_databases_dir = {hr_databases_dir} # HR databases directory\n")
-        f.write(f"hr_profielen_dir = {hr_profielen_dir} # HR profielen directory\n")
-        f.write(f"default_files_dir = {default_files_dir} # Default files directory\n")
-        f.write(f"database_bekleding_dir = {database_bekleding_dir} # Database bekleding directory\n")
-        f.write(f"steentoets_dir = {steentoets_dir} # Steentoets directory\n")
-        f.write(f"bag_gebouwen_dir = {bag_gebouwen_dir} # BAG gebouwen directory\n\n")
+        f.write(f"hr_databases_dir =    {hr_databases_dir}     # HR databases directory\n")
+        f.write(f"hr_profielen_dir =    {hr_profielen_dir}     # HR profielen directory\n")
+        f.write(f"default_files_dir =   {default_files_dir}   # Default files directory\n")
+        f.write(f"database_bekleding_dir =      {database_bekleding_dir}     # Database bekleding directory\n")
+        f.write(f"steentoets_dir =      {steentoets_dir}     # Steentoets directory\n")
+        f.write(f"bag_gebouwen_dir =    {bag_gebouwen_dir}     # BAG gebouwen directory\n\n")
 
-        f.write(f"vakindeling_dir = {vakindeling_dir} # Vakindeling directory\n")
-        f.write(f"hr_results_dir = {hr_results_dir} # HR results directory\n")
-        f.write(f"waterlevel_dir = {waterlevel_dir} # Waterlevel directory\n")
-        f.write(f"overflow_dir = {overflow_dir} # Overflow directory\n")
-        f.write(f"bekleding_dir = {bekleding_dir} # Bekleding directory\n")
-        f.write(f"profielen_dir = {profielen_dir} # Profielen directory\n")
-        f.write(f"ahn_profielen_dir = {ahn_profielen_dir} # AHN profielen directory\n")
-        f.write(f"kar_profielen_dir = {kar_profielen_dir} # KAR profielen directory\n")
-        f.write(f"repr_profielen_dir = {repr_profielen_dir} # REPR profielen directory\n")
-        f.write(f"teenlijn_dir = {teenlijn_dir} # Teenlijn directory\n")
-        f.write(f"bebouwing_dir = {bebouwing_dir} # Bebouwing directory\n")
+        f.write(f"vakindeling_dir =     {vakindeling_dir}       # Vakindeling directory\n")
+        f.write(f"hr_results_dir =     {hr_results_dir}     # HR results directory\n")
+        f.write(f"waterlevel_dir =      {waterlevel_dir}       # Waterlevel directory\n")
+        f.write(f"overflow_dir =        {overflow_dir}      # Overflow directory\n")
+        f.write(f"bekleding_dir =       {bekleding_dir}     # Bekleding directory\n")
+        f.write(f"profielen_dir =        {profielen_dir}    # Profielen directory\n")
+        f.write(f"ahn_profielen_dir = {ahn_profielen_dir}   # AHN profielen directory\n")
+        f.write(f"kar_profielen_dir = {kar_profielen_dir}   # KAR profielen directory\n")
+        f.write(f"repr_profielen_dir = {repr_profielen_dir}     # REPR profielen directory\n")
+        f.write(f"teenlijn_dir = {teenlijn_dir}     # Teenlijn directory\n")
+        f.write(f"bebouwing_dir = {bebouwing_dir}   # Bebouwing directory\n")
 
 
 
@@ -121,6 +121,6 @@ def create_project_structure(project_folder, traject_id):
     print(f"Project structure created for '{os.path.basename(project_folder)}'.")
 
 if __name__ == "__main__":
-    project_folder = "c:/vrm_test/test_folder_structure22"
-    traject_id = "7-2"
+    project_folder = "c:/vrm_test/test_folder_structure25"
+    traject_id = "18-4"
     create_project_structure(project_folder, traject_id)
