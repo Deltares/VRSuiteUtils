1698e9e4ee1007087c8b60465e72db6b16008fc5
Wouter Jan Klerk <wouterjan.klerk@deltares.nl>
2024-09-27 09:31:06 +0200

    feature: add geometry checks

--- preprocessing/step2_mechanism_data/revetments/revetment_slope.py
+++ preprocessing/step2_mechanism_data/revetments/revetment_slope.py
@@ -44,10 +44,34 @@ class RevetmentSlope:
             print(f'WARNING: Amp voor {self.dwarsprofiel} is niet ingevuld. Amp=0.0 aangenomen.')
 
     def add_steentoets(self, steentoets_path):
-        steentoets_df = read_steentoets_file(steentoets_path.joinpath(self.steentoetsfile), self.dwarsprofiel)
-        #add stone slope parts to the slope
-        self.slope_parts = []
-        for index, row in steentoets_df.iterrows():
-            self.slope_parts.append(SlopePart(row, self.doorsnede))
-
+        if hasattr(self, 'steentoetsfile'):
+            steentoets_df = read_steentoets_file(steentoets_path.joinpath(self.steentoetsfile), self.dwarsprofiel)
+            self.zst = True
+            #add stone slope parts to the slope
+            self.slope_parts = []
+            for index, row in steentoets_df.iterrows():
+                self.slope_parts.append(SlopePart(row, self.doorsnede))
+                #grass and stone
+            self.check_geometry()
+        else:
+            pass #only grass
         #TODO: should it crash if the stone slope part is below the end of the grass slope? This would mean that the input is inconsistent
+
+    def check_geometry(self):
+        def find_grass_slope_zo(slope_parts):
+            for slope_part in slope_parts:
+                if float(slope_part.toplaagtype) == 20.0: #TODO include other grass types?
+                    return slope_part.Zo
+                else:
+                    pass
+                    #not found return top
+            return slope_part.Zb
+        #the grass slope should not be below the stone slope
+        slope_grass_limit = find_grass_slope_zo(self.slope_parts)
+        if slope_grass_limit > self.begin_grasbekleding:
+            #steentoets is beschikbaar, dan geldt de overgangshoogte op basis van steentoets
+            self.begin_grasbekleding = slope_grass_limit
+            print(f'WARNING: Het opgegeven eind_grasbekleding ligt lager dan de overgang op basis van steentoets voor dwarsprofiel {self.dwarsprofiel}. Overgangshoogte uit Steentoets wordt gebruikt.')
+        elif slope_grass_limit < self.begin_grasbekleding:
+            self.begin_grasbekleding = slope_grass_limit
+            print(f'WARNING: Het opgegeven eind_grasbekleding ligt hoger dan de overgang op basis van steentoets voor dwarsprofiel {self.dwarsprofiel}. Overgangshoogte uit Steentoets wordt gebruikt.')
