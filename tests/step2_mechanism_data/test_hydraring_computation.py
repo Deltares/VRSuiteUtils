41aafafbda2db1cd688442362765a17af11f5105
srikkert <stephan.rikkert@deltares.nl>
2024-10-03 12:14:18 +0200

    cleaned old test code

--- tests/step2_mechanism_data/test_hydraring_computation.py
+++ tests/step2_mechanism_data/test_hydraring_computation.py
@@ -13,31 +13,6 @@ from preprocessing.step2_mechanism_data.hydraring_computation import HydraRingCo
 
 class TestHydraRingComputation:
 
-    # @pytest.mark.parametrize("values, betas", [
-    #     pytest.param([1, 2, 3, 5, 4], [0.1, 0.2, 0.3, 0.4, 0.5], id="unordered values"),
-    # ])
-    # def test_hydraring_input_with_unordered_values_fails(self, values, betas):
-    #     #check if it raises an error:
-    #     with pytest.raises(ValueError):
-    #         HydraRingComputation.check_and_justify_HydraRing_data(values, betas, "test", "test")
-
-    # @pytest.mark.parametrize("values, betas", [
-    #     pytest.param([1, 2, 3, 4, 5], [0.1, 0.2, 0.3, 0.4, 0.5], id="ordered values"),
-    # ])
-    # def test_hydraring_input_with_ordered_values_succeeds(self, values, betas):
-    #     #check if it succeeds:
-    #     HydraRingComputation.check_and_justify_HydraRing_data(values, betas, "test", "test")
-
-    # @pytest.mark.parametrize("values, betas", [
-    #     pytest.param([1, 2, 3, 4, 5], [0.1, 0.2, 0.3, 0.2, 0.5], id="non-increasing values"),
-    # ])
-    # def test_hydraring_input_with_non_increasing_values_succeeds(self, values, betas):
-    #     #check if it gives the right output:
-    #     values, betas = HydraRingComputation.check_and_justify_HydraRing_data(values, betas, "test", "test")
-    #     expected_betas = [0.1, 0.2, 0.3, 0.3, 0.5]
-    #     assert betas == expected_betas
-
-
     def test_hydraring_design_table_with_non_increasing_betas(self):
         # Set paths to the design tables
         before_correction_path = test_data.joinpath("13-6", "intermediate_results", "HR_results", "waterlevel", "beta_not_increasing", "BeforeCorrection_DESIGNTABLE_13-6_0003.txt")
