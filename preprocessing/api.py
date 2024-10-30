b5a985a5bfe4355d225d8b37bab50806e97bc3e5
srikkert <stephan.rikkert@deltares.nl>
2024-10-30 16:18:57 +0100

    Changed boolean
    Initially, a boolean was asked for the traject shape, so it could only handle False. If false, it would get the traject shape from the waterveiligheidsportaal WFS. However, we want to be able to give a path. So it should check for a string to an existing path, otherwise give a False
    
    To do: if trajectshape is not false, check if it is a path to an existing file. And does it have to be a shapefile or is geopackage also possible?

--- preprocessing/api.py
+++ preprocessing/api.py
@@ -85,8 +85,11 @@ def generate_vakindeling_shape(config_file: str, results_folder: Path = None):
         if output_folder_vakindeling.exists():
             output_folder_vakindeling.rmdir()
         output_folder_vakindeling.mkdir(parents=True, exist_ok=True)
-    
-    traject_shape = parameters.getboolean('traject_shapefile', fallback=False)  # set default value to False if not present
+
+    # get traject_shape from parameters. This is either a FALSE or none (or nothing filled in), or a pathname
+    traject_shape = parameters['traject_shapefile'] if 'traject_shapefile' in parameters else False
+
+    # traject_shape = parameters.getboolean('traject_shapefile', fallback=False)  # set default value to False if not present
     flip = parameters.getboolean('flip_traject', fallback=False)  # set default value to False if not present
 
     # print the parameters
@@ -320,6 +323,8 @@ def get_characteristic_profiles_for_traject(config_file: str, results_folder: Pa
         print(f"Error reading configuration: {e}")
         return
 
+
+
     # Accessing parameters
     traject_id = parameters['traject_id']
     if results_folder is None:
@@ -334,9 +339,11 @@ def get_characteristic_profiles_for_traject(config_file: str, results_folder: Pa
     dx = parameters.getint('dx', fallback=25)  # set default value to 25 if not present
     voorland_lengte = parameters.getint('voorland_lengte', fallback=50)  # set default value to 50 if not present
     achterland_lengte = parameters.getint('achterland_lengte', fallback=75)  # set default value to 75 if not present
-    traject_shape = parameters.getboolean('traject_shape', fallback=False)  # set default value to False if not present
+    traject_shape = parameters['traject_shapefile'] if 'traject_shapefile' in parameters else False
     flip_traject = parameters.getboolean('flip_traject', fallback=False)  # set default value to False if not present
     flip_waterkant = parameters.getboolean('flip_waterkant', fallback=False)  # set default value to False if not present
+    # get traject_shape from parameters. This is either a FALSE or none (or nothing filled in), or a pathname
+
 
     # print the parameters
     print("\nDe volgende parameters zijn gelezen uit het configuratiebestand:\n")
